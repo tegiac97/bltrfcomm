30b46e097d520732734f4571a85c3a2b5546de71
lebo <lehaichau.22@gmail.com>
2019-12-18 13:06:55 +0700

    rfcomm server

---
+++ server.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
+
+int main(int argc, char **argv)
+{
+    struct sockaddr_rc loc_addr = { 0 }, client_addr = { 0 };
+    char buf[1024] = { 0 };
+    int s, client, bytes_read;
+    socklen_t opt = sizeof(client_addr);
+
+    // allocate socket
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+    loc_addr.rc_family = AF_BLUETOOTH;
+    // loc_addr.rc_bdaddr = *BDADDR_ANY;
+    loc_addr.rc_channel = (uint8_t) 1;
+    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+
+    // put socket into listening mode
+    listen(s, 1);
+
+    client = accept(s, (struct sockaddr *)&client_addr, &opt);
+
+    str2ba("74:40:BB:03:D9:C4", &loc_addr.rc_bdaddr);
+    fprintf(stderr, "accepted connection from %s\n", buf);
+    memset(buf, 0, sizeof(buf));
+
+    // read data from the client
+    bytes_read = read(client, buf, sizeof(buf));
+    if( bytes_read > 0 ) {
+        printf("received [%s]\n", buf);
+        write(client, "sdjhfgdsjhgfhsd", 15);
+    }
+
+    // close connection
+    close(client);
+    close(s);
+    return 0;
+}
